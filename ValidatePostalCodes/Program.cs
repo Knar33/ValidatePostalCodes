6e9c394a710ed1769332afc3630df662da43f1a1
Knar <knarxr@protonmail.com>
2018-03-21 06:43:12 -0500

    Initial Upload

---
+++ ValidatePostalCodes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatePostalCodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PostalCodes> postalCodes = new List<PostalCodes>();
+            using (StreamReader reader = new StreamReader("allcountries.txt"))
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    string[] parts = line.Split('\t');
+                    postalCodes.Add(new PostalCodes(parts[0], parts[1]));
+                }
+            }
+            if (!(postalCodes.Where(x => x.Country == "US" && x.PostalCode == "12345").Count() > 0))
+            {
+                Console.WriteLine("contains");
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class PostalCodes
+    {
+        public PostalCodes(string country, string postalCode)
+        {
+            Country = country;
+            PostalCode = postalCode;
+        }
+
+        public string Country { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
