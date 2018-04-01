53c90ec61dbdc8c8669687328b124ac3df80758c
Knar33 <adbyerly@comcast.net>
2018-04-01 13:00:03 -0500

    Broke everything into separate classes.

---
+++ ValidatePostalCodes/PostalCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatePostalCodes
+{
+    public class PostalCode
+    {
+        public PostalCode(string country, string code)
+        {
+            Country = country;
+            Code = code;
+        }
+
+        public string Country { get; set; }
+
+        public string Code { get; set; }
+    }
+}
---
+++ ValidatePostalCodes/PostalCodeValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatePostalCodes
+{
+    public class PostalCodeValidator
+    {
+        public PostalCodeValidator()
+        {
+            Codes = new List<PostalCode>();
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "ValidatePostalCodes.allCountries.txt";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    string[] parts = line.Split('\t');
+                    Codes.Add(new PostalCode(parts[0], parts[1]));
+                }
+            }
+        }
+
+        public bool Validate(string postalCode, string countryCode)
+        {
+            if (Codes.Count(x => x.Country == countryCode && x.Code == postalCode) == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public List<PostalCode> Codes { get; set; }
+
+        public List<PostalCodeFormat> Formats { get; set; }
+    }
+}
