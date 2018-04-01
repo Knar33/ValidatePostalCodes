305b8427d40b0a54d9b1502fd091ed028bda5a0a
Knar <knarxr@protonmail.com>
2018-04-01 13:00:03 -0500

    Broke everything into separate classes.

--- ValidatePostalCodes/Program.cs
+++ ValidatePostalCodes/Program.cs
@@ -12,43 +12,10 @@ namespace ValidatePostalCodes
     {
         static void Main(string[] args)
         {
-            List<PostalCodes> postalCodes = new List<PostalCodes>();
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = "ValidatePostalCodes.allCountries.txt";
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                while (true)
-                {
-                    string line = reader.ReadLine();
-                    if (line == null)
-                    {
-                        break;
-                    }
-                    string[] parts = line.Split('\t');
-                    postalCodes.Add(new PostalCodes(parts[0], parts[1]));
-                }
-            }
+            PostalCodeValidator postalCodeValidator = new PostalCodeValidator();
+            postalCodeValidator.Validate("60187", "US");
 
-            if (postalCodes.Where(x => x.PostalCode == "2625").Count() == 0)
-            {
-                Console.WriteLine("Does not contain");
-            }
             Console.ReadLine();
-
         }
-    }
-
-    public class PostalCodes
-    {
-        public PostalCodes(string country, string postalCode)
-        {
-            Country = country;
-            PostalCode = postalCode;
-        }
-
-        public string Country { get; set; }
-
-        public string PostalCode { get; set; }
     }
 }
